cac3fa7f5d975736a78cd8421462ba0e30d5d34c
Felipe Mazieri <felipe.a.mazieri@gmail.com>
2022-01-08 01:29:15 -0300

    add favo icon + setState

--- lib/app/view/home/lista.dart
+++ lib/app/view/home/lista.dart
@@ -1,25 +1,122 @@
 import 'package:flutter/material.dart';
 
-class Lista extends StatelessWidget {
+import 'package:flutter_svg/svg.dart';
+
+class Lista extends StatefulWidget {
   const Lista({Key? key}) : super(key: key);
+
+  @override
+  State<Lista> createState() => _ListaState();
+}
+
+class _ListaState extends State<Lista> {
+  var _corBookmark = "assets/icons/bookmark_on.svg";
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      itemCount: 13,
+      itemCount: 6,
       padding: const EdgeInsets.all(0),
       separatorBuilder: (context, index) => Divider(
         color: Colors.white.withOpacity(0),
       ),
       itemBuilder: (context, index) {
-        return SizedBox(
+        return Container(
           height: 275,
-          child: Image.asset(
-            "assets/img/3.jpg",
-            fit: BoxFit.cover,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                        ),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            "assets/img/${index < 3 ? index + 1 : index > 3 ? index - 2 : 1}.jpg",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(12),
+                          bottomRight: Radius.circular(12),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [],
+                          ),
+                          Row(
+                            children: [],
+                          ),
+                          Row(
+                            children: [],
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Align(
+                alignment: const Alignment(1, 0.15),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: SvgPicture.asset(
+                    "assets/icons/navigation.svg",
+                    color: const Color(0xFF4668d9),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<CircleBorder>(
+                      const CircleBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: const Alignment(0.925, -0.9),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _corBookmark =
+                          _corBookmark == "assets/icons/bookmark_on.svg"
+                              ? "assets/icons/bookmark_off.svg"
+                              : "assets/icons/bookmark_on.svg";
+                    });
+                  },
+                  child: SvgPicture.asset(
+                    index % 2 == 0
+                        ? _corBookmark
+                        : _corBookmark == "assets/icons/bookmark_on.svg"
+                            ? "assets/icons/bookmark_off.svg"
+                            : "assets/icons/bookmark_on.svg",
+                  ),
+                ),
+              ),
+            ],
           ),
         );
       },
     );
   }
 }
+
+// index < 3 ? index + 1 : index > 3 ? index - 2 : 1
